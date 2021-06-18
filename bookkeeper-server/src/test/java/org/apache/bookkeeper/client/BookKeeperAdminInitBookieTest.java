92cbdd42614011db9bd40a1b90f216d210f53e52
chiacchius <matteoch99@gmail.com>
2021-06-18 17:18:35 +0200

    improve tests

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/client/BookKeeperAdminInitBookieTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/BookKeeperAdminInitBookieTest.java
@@ -1,13 +1,14 @@
 package org.apache.bookkeeper.client;
 
+import org.apache.bookkeeper.bookie.BookieException;
 import org.apache.bookkeeper.bookie.BookieImpl;
 import org.apache.bookkeeper.bookie.storage.ldb.DbLedgerStorage;
 import org.apache.bookkeeper.common.allocator.PoolingPolicy;
 import org.apache.bookkeeper.conf.BookKeeperClusterTestCase;
 import org.apache.bookkeeper.conf.ServerConfiguration;
+import org.apache.bookkeeper.discover.RegistrationManager;
 import org.apache.bookkeeper.meta.zk.ZKMetadataDriverBase;
-import org.apache.bookkeeper.test.ZooKeeperCluster;
-import org.apache.bookkeeper.test.ZooKeeperClusterUtil;
+import org.apache.bookkeeper.net.BookieId;
 import org.apache.bookkeeper.util.BookKeeperConstants;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
@@ -18,9 +19,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.io.File;
-import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Collection;
+
+import static org.apache.bookkeeper.meta.MetadataDrivers.runFunctionWithRegistrationManager;
 
 
 /**
@@ -31,28 +33,33 @@ public class BookKeeperAdminInitBookieTest extends BookKeeperClusterTestCase{
 
     boolean expectedResult;
     private static final int numOfBookies = 2;
-    private boolean useServerConfiguration;
-    private int bookieindex = 0;
+    private final boolean useServerConfiguration;
+    private final int bookieindex = 0;
     private ServerConfiguration sv = null;
 
     @Before
     public void setUp() throws Exception {
-
         super.setUp();
-
+        //kill a potential existing bookie
         if (useServerConfiguration) sv = confByIndex(bookieindex);
 
+        System.out.println("starting setUp");
+        try {
+            killBookie(bookieindex);
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("there is no bookie to kill");
+        }
 
         File[] journalDirs = sv.getJournalDirs();
         for (File journalDir : journalDirs) {
             FileUtils.deleteDirectory(journalDir);
         }
 
-
         File[] ledgerDirs = sv.getLedgerDirs();
         for (File ledgerDir : ledgerDirs) {
             FileUtils.deleteDirectory(ledgerDir);
         }
+
 
         File[] indexDirs = sv.getIndexDirs();
         if (indexDirs != null) {
@@ -63,11 +70,17 @@ public class BookKeeperAdminInitBookieTest extends BookKeeperClusterTestCase{
 
 
 
+
+
+
+
+
     }
 
     @After
     public void tearDown() throws Exception {
 
+        //kill the created bookie
         killBookie(bookieindex);
 
 
@@ -81,7 +94,7 @@ public class BookKeeperAdminInitBookieTest extends BookKeeperClusterTestCase{
     @Parameterized.Parameters
     public static Collection<?> getParameters(){
         return Arrays.asList(new Object[][] {
-                //expected result, useServerConfiguratio
+                //expected result, useServerConfiguration
                 {true, true}
                 //{true, data, 2, data.length-2, true, sync},
         });
@@ -96,6 +109,7 @@ public class BookKeeperAdminInitBookieTest extends BookKeeperClusterTestCase{
 
     @Test
     public void initBookie() throws Exception {
+        System.out.println("starting test");
 
         String bookieId = BookieImpl.getBookieId(sv).toString();
         String bookieCookiePath =
@@ -104,30 +118,11 @@ public class BookKeeperAdminInitBookieTest extends BookKeeperClusterTestCase{
                         + "/" + bookieId;
         zkc.delete(bookieCookiePath, -1);
 
-        if (!BookKeeperAdmin.initBookie(sv)) killBookie(bookieindex);
-
-
         boolean result = BookKeeperAdmin.initBookie(sv);
 
         Assert.assertEquals(expectedResult, result);
+        System.out.println("test finished");
 
-
-
-    }
-
-
-
-    private void doServerConfig(ServerConfiguration serverConfiguration) {
-        serverConfiguration.setJournalFlushWhenQueueEmpty(true);
-        serverConfiguration.setAllowEphemeralPorts(true);
-        serverConfiguration.setJournalFormatVersionToWrite(5);
-        serverConfiguration.setBookiePort(0);
-        serverConfiguration.setGcWaitTime(1000);
-        serverConfiguration.setDiskUsageWarnThreshold(0.99f);
-        serverConfiguration.setDiskUsageThreshold(0.999f);
-        serverConfiguration.setProperty(DbLedgerStorage.READ_AHEAD_CACHE_MAX_SIZE_MB, 4);
-        serverConfiguration.setAllocatorPoolingPolicy(PoolingPolicy.UnpooledHeap);
-        serverConfiguration.setProperty(DbLedgerStorage.WRITE_CACHE_MAX_SIZE_MB, 4);
 
     }
 
