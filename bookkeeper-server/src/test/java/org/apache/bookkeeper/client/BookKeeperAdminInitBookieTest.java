86e1a8ec7749a4e5b6de46074a4aad77bcbfffaa
chiacchius <matteoch99@gmail.com>
2021-06-19 16:46:20 +0200

    mutuation done

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/client/BookKeeperAdminInitBookieTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/BookKeeperAdminInitBookieTest.java
@@ -1,13 +1,15 @@
 package org.apache.bookkeeper.client;
 
 
-import com.google.common.util.concurrent.UncheckedExecutionException;
 import org.apache.bookkeeper.bookie.BookieImpl;
 
 import org.apache.bookkeeper.conf.BookKeeperClusterTestCase;
 import org.apache.bookkeeper.conf.ServerConfiguration;
 import org.apache.bookkeeper.meta.zk.ZKMetadataDriverBase;
+import org.apache.bookkeeper.net.BookieId;
 import org.apache.bookkeeper.util.BookKeeperConstants;
+import org.apache.bookkeeper.versioning.Version;
+import org.apache.bookkeeper.versioning.Versioned;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Assert;
@@ -17,10 +19,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Collection;
 
-
+import static org.apache.bookkeeper.meta.MetadataDrivers.runFunctionWithRegistrationManager;
 
 
 @RunWith(value = Parameterized.class)
@@ -30,11 +33,12 @@ public class BookKeeperAdminInitBookieTest extends BookKeeperClusterTestCase{
 
     private final boolean deleteDirs;
 
-
+    private boolean writeBookieData;
     private static final int numOfBookies = 2;
     private final boolean useServerConfiguration;
     private final int bookieindex = 0;
     private ServerConfiguration sv = null;
+    private boolean removeBookie;
 
     @Before
     public void setUp() throws Exception {
@@ -47,17 +51,9 @@ public class BookKeeperAdminInitBookieTest extends BookKeeperClusterTestCase{
         if (useServerConfiguration) sv = confByIndex(bookieindex);
 
         System.out.println("starting setUp");
-        try {
-            killBookie(bookieindex);
-        }catch (IndexOutOfBoundsException e){
-            System.out.println("there is no bookie to kill");
-        }
+
         if (sv!=null && this.deleteDirs) {
 
-            File[] journalDirs = sv.getJournalDirs();
-            for (File journalDir : journalDirs) {
-                FileUtils.deleteDirectory(journalDir);
-            }
             File[] ledgerDirs = sv.getLedgerDirs();
             for (File ledgerDir : ledgerDirs) {
                 FileUtils.deleteDirectory(ledgerDir);
@@ -68,23 +64,27 @@ public class BookKeeperAdminInitBookieTest extends BookKeeperClusterTestCase{
                     FileUtils.deleteDirectory(indexDir);
                 }
             }
+            File[] journalDirs = sv.getJournalDirs();
+            for (File journalDir : journalDirs) {
+                FileUtils.deleteDirectory(journalDir);
+            }
 
         }
 
 
-
-
-
-
-
-
     }
+
+
 
     @After
     public void tearDown() throws Exception {
 
         //kill the created bookie
-        killBookie(bookieindex);
+        try {
+            killBookie(bookieindex);
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("IndexOutOfBoundsException");
+        }
         super.tearDown();
 
 
@@ -92,25 +92,28 @@ public class BookKeeperAdminInitBookieTest extends BookKeeperClusterTestCase{
     }
 
 
-
-
     //input parameters
     @Parameterized.Parameters
     public static Collection<?> getParameters(){
         return Arrays.asList(new Object[][] {
-                //expected result, useServerConfiguration, deleteDirs
-                {true, true, true},
-                {false, false, true},
-                {false, true, false},
+                //expected result, useServerConfiguration, deleteDirs, writeBookieData, removeBookie
+                {true, true, true, false, true},
+                {false, false, true, false, true},
+                {false, true, false, true, false},
+                {false, true, true, true, false},
+                {false, true, true, false, false},
+                {false, true, true, true, true}
 
         });
     }
 
-    public BookKeeperAdminInitBookieTest(boolean expectedResult, boolean useServerConfiguration, boolean deleteDirs) {
+    public BookKeeperAdminInitBookieTest(boolean expectedResult, boolean useServerConfiguration, boolean deleteDirs, boolean writeBookieData, boolean removeBookie) {
         super(numOfBookies);
         this.useServerConfiguration = useServerConfiguration;
         this.expectedResult = expectedResult;
         this.deleteDirs = deleteDirs;
+        this.writeBookieData = writeBookieData;
+        this.removeBookie = removeBookie;
 
     }
 
@@ -120,19 +123,40 @@ public class BookKeeperAdminInitBookieTest extends BookKeeperClusterTestCase{
         boolean result = false;
 
         if (sv!=null) {
-            String bookieId = BookieImpl.getBookieId(sv).toString();
-            String bookieCookiePath =
-                    ZKMetadataDriverBase.resolveZkLedgersRootPath(sv)
-                            + "/" + BookKeeperConstants.COOKIE_NODE
-                            + "/" + bookieId;
-            zkc.delete(bookieCookiePath, -1);
-        try {
+
+            BookieId bookieId = BookieImpl.getBookieId(sv);
+            if (removeBookie) {
+                try {
+                    killBookie(bookieindex);
+                }catch (IndexOutOfBoundsException e){
+                    System.out.println("there is no bookie to kill");
+                }
+                String bookieCookiePath =
+                        ZKMetadataDriverBase.resolveZkLedgersRootPath(sv)
+                                + "/" + BookKeeperConstants.COOKIE_NODE
+                                + "/" + bookieId.toString();
+                zkc.delete(bookieCookiePath, -1);
+            }
+
+            if (this.writeBookieData) {
+                runFunctionWithRegistrationManager(sv, rm -> {
+                    Versioned<byte[]> cookieData = new Versioned<>("test".getBytes(StandardCharsets.UTF_8), Version.NEW);
+                    try {
+                        rm.writeCookie(bookieId, cookieData);
+
+                    } catch (Exception e) {
+
+                    }
+                    return null;
+                });
+            }
+
+            try {
                  result = BookKeeperAdmin.initBookie(sv);
             } catch (Exception e) {
                 result =false;
 
             }
-
 
         }
 
